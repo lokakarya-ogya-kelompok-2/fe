75072a925df44d3af9990795daa9165d657dabc9
kharis <kharisrahmat686@gmail.com>
2024-11-22 13:27:22 +0700

    feat: add few pages, adjust route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { IndexPageComponent } from './pages/index-page/index-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-import { MenuPageComponent } from './pages/menu-page/menu-page.component';
 import { ManageDivisionComponent } from './pages/manage-division/manage-division.component';
 import { ManageGroupAttitudeSkillComponent } from './pages/manage-group-attitude-skill/manage-group-attitude-skill.component';
+import { MenuPageComponent } from './pages/menu-page/menu-page.component';
+
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent, IndexPageComponent, MainPageComponent, MenuPageComponent, ManageDivisionComponent, ManageGroupAttitudeSkillComponent],
+  imports: [
+    RouterOutlet,
+    LoginPageComponent,
+    IndexPageComponent,
+    MainPageComponent,
+    ManageDivisionComponent,
+    ManageGroupAttitudeSkillComponent,
+    MenuPageComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'fe';
