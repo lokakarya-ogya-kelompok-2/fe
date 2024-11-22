730396f6435a707c03240e1fbde3a4cd5c47ddad
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-11-22 14:19:55 +0700

    feat: add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,6 @@ import { LoginComponent } from './features/auth/login/components/login.component
 import { ManageDivisionComponent } from './features/divisions/components/manage-division.component';
 import { ManageGroupAttitudeSkillComponent } from './features/group-attitude-skill/components/manage-group-attitude-skill.component';
 import { IndexPageComponent } from './features/index/components/index-page.component';
-import { ManageComponent } from './features/manage/components/manage-page.component';
 import { MainPageComponent as MenuComponent } from './features/menu/components/main-page.component';
 import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
 export const routes: Routes = [
@@ -16,7 +15,7 @@ export const routes: Routes = [
   { path: 'menu', component: MenuComponent, canActivate: [authGuard] },
   {
     path: 'manage',
-    component: ManageComponent,
+    // component: ManageComponent,
     children: [
       { path: '', component: ManageDivisionComponent },
       {
