0f6fb8c6354d2a2ba24033a1d72963d0a0d4b686
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-12-05 15:03:35 +0700

    feat: display empty input field on empty group

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,10 +16,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter, withRouterConfig } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { dateInterceptor } from './core/interceptors/date.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor, dateInterceptor])
+    ),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withRouterConfig({ onSameUrlNavigation: 'reload' })),
     provideClientHydration(),
