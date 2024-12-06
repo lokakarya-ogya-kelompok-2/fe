84f7bf5e1c50dcb532d75e36dc2f2e559c6cb70d
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-12-06 10:48:58 +0700

    feat: ttd attitude-skill, lupa

--- src/app/features/group-attitude-skill/models/group-attitude-skill.ts
+++ src/app/features/group-attitude-skill/models/group-attitude-skill.ts
@@ -1,9 +1,11 @@
 import { ResponseContentMeta } from '../../../shared/models/response-content-meta';
+import { AttitudeSkill } from '../../attitude-skill/models/attitude-skill';
 
 export interface GroupAttitudeSkill extends ResponseContentMeta {
   id: string;
   group_name: string;
   percentage: number;
+  attitude_skills?: AttitudeSkill[];
   enabled: boolean;
 }
 
