368e48be40148aa8fadde49e3d02ae2829447081
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-12-18 22:23:34 +0700

    refactor: hapus console log gapenting

--- src/app/core/guards/no-auth.guard.ts
+++ src/app/core/guards/no-auth.guard.ts
@@ -4,7 +4,6 @@ import { map } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 export const noAuthGuard: CanActivateFn = (route, state) => {
-  console.log('DARI GUARD-NYA');
   const authService = inject(AuthService);
   const router = inject(Router);
   return authService.isAuthenticated$.pipe(
