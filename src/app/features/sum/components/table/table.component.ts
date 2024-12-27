fdaf95eecce0f366ae343dedca10bb2d2bf655eb
kharis <kharisrahmat686@gmail.com>
2024-12-27 11:49:59 +0700

    fix: bug

--- src/app/features/sum/components/table/table.component.ts
+++ src/app/features/sum/components/table/table.component.ts
@@ -1,16 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { TreeTableModule } from 'primeng/treetable';
 import { SummaryItem } from '../../models/summary';
-
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [TableModule, CommonModule],
+  imports: [TableModule, CommonModule, TreeTableModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
 export class TableComponent {
   @Input() data: SummaryItem[] = [];
+  // files: any[] = [];
   @Input() tableHeader: string = '';
+  // ngOnInit() {
+  //   // Mock data
+  //   this.files = [
+  //     {
+  //       data: {
+  //         // rowData
+  //         key: '0',
+  //         group_name: 'Group 1',
+  //         percentage: 30,
+  //         type: 'Group',
+  //       },
+  //       children: [
+  //         {
+  //           key: '0-0',
+  //           group_name: 'Group group',
+  //           percentage: 40,
+  //           type: 'Group',
+  //         },
+  //       ], // rowNode.children
+  //       expanded: false, // rowNode.expanded
+  //       leaf: false, // rowNode.leaf
+  //     },
+  //   ];
+  //   console.log(this.files);
+  // }
 }
